913b8dc532959156978f7e399273e6891cd29c11
Violetta Szewczyk <szewczykviolet@gmail.com>
2022-02-07 14:19:41 -0600

    leetcode389

--- findTheDifference.js
+++ findTheDifference.js
@@ -1,9 +1,13 @@
-var findTheDifference = function(s, t) {
-    for(let i = 0; i < t.length; i++){
-        if(!s.includes(t[i])){
-            return t[i];
+var findTheDifference = function(str1, str2) {
+    for(var i = 0; i < str2.length; i++){
+        var placer = str1.indexOf(str2[i])
+        if(placer >= 0){
+            str2 = str2.slice(0,i) + str2.slice(i+1)
+            str1 = str1.slice(0,placer) + str1.slice(placer+1)
+            i--
         }
     }
-};
+    return str2
+}
 
 console.log(findTheDifference("abcd", "abcde"))
